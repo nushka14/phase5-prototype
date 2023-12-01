70201a8669ed3939a9834d0c280ec2913ede756b
nushka14 <100300249+nushka14@users.noreply.github.com>
2023-11-30 22:43:32 -0700

    Update phase6.java

--- phase6.java
+++ phase6.java
@@ -1,61 +1,29 @@
-package com.phase6;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Map;
+public class Main extends Application {
 
-public class phase5 {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Retrieve Historical Data");
+        btn.setOnAction(event -> System.out.println("Historical Data Retrieved!"));
 
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final Map<String, String> historicalData = new HashMap<>();
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+        scene.getStylesheets().add("style.css");
+
+        primaryStage.setTitle("EffortLogger");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to EffortLogger!");
-
-        // Simulate user onboarding
-        System.out.print("Do you have an account? (yes/no): ");
-        String hasAccount = scanner.nextLine().trim().toLowerCase();
-        
-        if ("no".equals(hasAccount)) {
-            System.out.println("Creating a new account...");
-            // Further account creation logic would go here
-        } else {
-            System.out.println("Please log in...");
-            // Further login logic would go here
-        }
-
-        // Display user dashboard
-        System.out.println("User dashboard displayed...");
-
-        // Simulate historical data retrieval
-        loadHistoricalData(); // Simulate loading historical data
-        System.out.print("Enter a keyword to search for relevant historical projects: ");
-        String keyword = scanner.nextLine().trim();
-
-        searchHistoricalData(keyword); // Perform search based on keyword
-
-        scanner.close();
-    }
-
-    private static void loadHistoricalData() {
-        // Simulate loading historical data from a database
-        historicalData.put("Project Alpha", "Implemented login feature with two-factor authentication.");
-        historicalData.put("Project Beta", "Designed and deployed microservice architecture.");
-        historicalData.put("Project Gamma", "Refactored database schema for performance improvements.");
-    }
-
-    private static void searchHistoricalData(String keyword) {
-        System.out.println("Searching for projects related to: " + keyword);
-        // Simple keyword search in historical project descriptions
-        boolean found = false;
-        for (Map.Entry<String, String> entry : historicalData.entrySet()) {
-            if (entry.getValue().toLowerCase().contains(keyword.toLowerCase())) {
-                System.out.println("Found related project: " + entry.getKey() + " - " + entry.getValue());
-                found = true;
-            }
-        }
-        if (!found) {
-            System.out.println("No related projects found.");
-        }
+        launch(args);
     }
 }
